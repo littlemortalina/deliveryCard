97f44800e80496e86b0aaa143a6ae07f9ba1db20
Alina <al89056667426@gmail.com>
2025-11-06 13:12:11 +0300

    Test

---
+++ src/test/java/ru/netology/deliveryCardTest.java
@@ -0,0 +1,36 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+
+
+public class deliveryCardTest {
+
+    public String generateDate(int days, String pattern) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    void shouldSendForm() {
+        Selenide.open("http://0.0.0.0:9999");
+        $("[data-test-id=city] input").setValue("Москва");
+        String planDate = generateDate(5, "dd.MM.yyyy");
+        $("[data-test-id=date] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE).setValue(planDate);
+        $("[data-test-id=name] input").setValue("Иван Иванов");
+        $("[data-test-id=phone] input").setValue("+79067891289");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $(".notification__content")
+                .should(Condition.visible, Duration.ofSeconds(15))
+                .should(Condition.text("Встреча успешно забронирована на " + planDate));
+    }
+}
+
